3b06a7c45b8991f05dd0399bd38f4d42aa52d643
José Gómez <gomez.jose@correo.unimet.edu.ve>
2023-02-04 22:47:50 -0400

    Validación del formulario en JavaScript

--- main.js
+++ main.js
@@ -44,3 +44,22 @@ function nextSlide() {
     clearInterval(interval) 
     moveCarousel()
 }
+
+// Funcionalidad del formulario
+
+document.getElementById("enviarForm").onclick = function(){
+    x1 = true
+    x2 = true
+    x3 = true
+
+    const el1 = document.getElementById("nombre").value;
+    const el2 = document.getElementById("correo").value;
+    const el3 = document.getElementById("mensaje").value;
+
+    document.getElementById("nombre").value !== "" ? el1:x1 = false;
+    document.getElementById("correo").value !== "" ? el2:x2 = false;
+    document.getElementById("mensaje").value !== "" ? el3:x3 = false;
+
+    x1 === true && x2 === true && x3 === true ? alert(`Nombre: ${el1} \nCorreo: ${el2} \nMensaje: ${el3} \n\nSu solicitud ha sido enviada`): alert("Introduzca los datos correctamente")
+    // x1 === true && x2 === true && x3 === true ? alert("")
+}
